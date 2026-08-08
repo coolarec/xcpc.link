6bf465dd09cfa3f7f921094c9f6f903c923e9d8e
coolarec <coolarec@qq.com>
2026-08-08 12:13:41 +0800

    feat: 更新 Node.js 版本至 22.12.0，修改测试用例中的文本内容

--- src/views/home/components/homeComponents.test.ts
+++ src/views/home/components/homeComponents.test.ts
@@ -382,12 +382,12 @@ describe('home header controls', () => {
     expect(tickerBanners.map((banner) => banner.props('item'))).toEqual([
       {
         label: '赛战博弈',
-        text: '从夯到拉锐评2025赛季ICPC/CCPC各赛站队伍强度',
+        text: '从夯到拉锐评2025赛季ICPC/CCPC各赛站队伍强度 - 俊杰Charles',
         href: 'https://www.bilibili.com/video/BV1fkPuzJE6t',
       },
       {
         label: '赛战博弈',
-        text: '从夯到拉锐评2025赛季ICPC/CCPC各赛站体验',
+        text: '从夯到拉锐评2025赛季ICPC/CCPC各赛站体验 - 俊杰Charles',
         href: 'https://www.bilibili.com/video/BV12XfiBfEhG',
       },
     ])
